bbe274b77351600bd8bb800dc7475e4a9778cb19
uzoma <chidumaga@gmail.com>
2023-12-08 04:46:36 +0100

    begins implementing placing objects in garden

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { images } from '../utils/default-images'
 
-const Modal = ({ setIsModalOpen }) => {
+const Modal = ({ setIsModalOpen, addImageToGarden }) => {
 
     const modalStyles = {
-        background: `rgba(0, 0, 0, 0.85)`,
+        background: `rgba(34, 34, 34, 0.9)`,
         width: '45%',
         height: '100%',
         overflow: 'scroll',
         position: 'absolute',
         top: 0,
-        right: 0
+        right: 0,
+        zIndex: 99
     }
 
     const closeButtonStyles = {
@@ -27,6 +28,11 @@ const Modal = ({ setIsModalOpen }) => {
         justifyContent: 'center'
     }
 
+    const selectImage = (imageUrl) => {
+        addImageToGarden(imageUrl)
+        setIsModalOpen(false)
+    }
+
     return (
         <div style={modalStyles}>
             <button style={closeButtonStyles} onClick={() => setIsModalOpen(false)}>
@@ -34,9 +40,14 @@ const Modal = ({ setIsModalOpen }) => {
             </button>
             <div style={imagesContainer}>
                 {
-                    images.map((image) => 
+                    images.map((imageUrl) => 
                         <div style={{width: '40%', padding: '2.5%'}}>
-                            <img style={{maxWidth: '100%'}} src={image} alt='' />
+                            <img 
+                                style={{maxWidth: '100%', cursor: 'pointer'}} 
+                                src={imageUrl} 
+                                alt='' 
+                                onClick={() => selectImage(imageUrl)}
+                            />
                         </div>
                 )}
             </div>
--- src/screens/garden.jsx
+++ src/screens/garden.jsx
@@ -1,22 +1,41 @@
 import React, { useState } from 'react'
 import Modal from '../components/modal'
+import Draggable from 'react-draggable'
 
 const Garden = ({ setCurrentScreenName, screenNames }) => {
 
     const [ isModalOpen, setIsModalOpen ] = useState(true)
+    const [ gardenImages, setGardenImages ] = useState([])
 
     const addButtonStyle = {
-        position: 'absolute',
+        position: 'fixed',
         bottom: '15px',
         right: '15px',
         fontSize: '24px',
     }
+
+    const imageStyle = {
+        position: 'absolute',
+        top: '40%',
+        left: '40%',
+        maxWidth: '200px'
+    }
+
+    const addImageToGarden = (image) => {
+        setGardenImages([...gardenImages, image])
+    }
+
   return (
     <div>
         <button style={addButtonStyle} onClick={() => setIsModalOpen(true)}>
             <span role='img' aria-label='plus sign emoji'>➕</span>
         </button>
-        { isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
+
+        { isModalOpen && <Modal setIsModalOpen={setIsModalOpen} addImageToGarden={addImageToGarden} /> }
+
+        {
+            gardenImages.map((image) => <Draggable><img style={imageStyle} src={image} alt='' /></Draggable>)
+        }
     </div>
   )
 }
