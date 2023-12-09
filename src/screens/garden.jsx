927a849a9c937866fe0b6591ee7cf68e32b9e9b8
uzoma <chidumaga@gmail.com>
2023-12-09 01:08:24 +0100

    attempts adding resize functionality

--- src/screens/garden.jsx
+++ src/screens/garden.jsx
@@ -16,12 +16,19 @@ const Garden = ({ setCurrentScreenName, screenNames }) => {
         zIndex: 99
     }
 
-    const imageStyle = {
+    const imageContainerStyle = {
         position: 'absolute',
         top: '40%',
         left: '40%',
         maxWidth: '200px',
+        resize: 'both',
+        overflow: 'hidden',
         // boxShadow: `0px 20px 4px rgba(0,0,0,0.25)`
+    }
+
+    const imageStyle = {
+        width: '100%',
+        height: '100%'
     }
 
     const addImageToGarden = (image) => {
@@ -42,8 +49,13 @@ const Garden = ({ setCurrentScreenName, screenNames }) => {
         { isModalOpen && <Modal setIsModalOpen={setIsModalOpen} addImageToGarden={addImageToGarden} /> }
 
         {
-            gardenImages.map((imageUrl) => <Draggable onStart={(event) => foregroundImage(event)}><img style={imageStyle} src={imageUrl} alt='' /></Draggable>)
-        }
+            gardenImages.map((imageUrl) => 
+                <Draggable onStart={(event) => foregroundImage(event)}>
+                    <div style={imageContainerStyle}>
+                        <img style={imageStyle} src={imageUrl} alt='' />
+                    </div>
+                </Draggable>
+        )}
     </div>
   )
 }
